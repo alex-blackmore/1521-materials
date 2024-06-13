17acd2d82b12173b3446f68d4addbc40c3f13f75
Alex Blackmore <ablackmore113@gmail.com>
2024-06-13 15:58:15 +1000

    question files

---
+++ wk4/q5.s
@@ -0,0 +1,23 @@
+	.text
+sum4:
+sum4__prologue:
+
+sum4_body:
+
+sum4_epilogue:
+	jr	$ra
+
+sum2:
+sum2__prologue:
+
+sum2_body:
+
+sum2_epilogue:
+	jr	$ra
+
+main:
+main__prologue:
+main__body:
+
+main__epilogue:
+	jr	$ra
