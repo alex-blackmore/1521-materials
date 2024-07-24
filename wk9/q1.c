7989393d65ef28665f8a5093b20d1a353a144c33
Alex Blackmore <ablackmore113@gmail.com>
2024-07-24 22:26:58 +1000

    w9 not at the last minute

---
+++ wk9/q1.c
@@ -0,0 +1,10 @@
+// print contents of ~/.diary
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char const *basename = ".diary";
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
---
+++ wk9/q5.c
@@ -0,0 +1,10 @@
+// read 32-byte hexadecimal numbers from a file
+// and print low (least significant) byte
+// as a signed decimal number (-128..127)
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
